4eca626a1a59ba37edf3152645920ed1aa8406b8
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-09-11 13:36:29 +0600

    solve fizzbuzz challenge

---
+++ Module-1/FizzBuzz.py
@@ -0,0 +1,11 @@
+def FizzBuzz(number):
+    if number%3 == 0 and number%5 ==0:
+        return "fizzbuzz"
+    elif number%3 == 0:
+        return "fizz"
+    elif number%5 == 0:
+        return "buzz"
+    else: return ""
+
+print(FizzBuzz(15))
+    
