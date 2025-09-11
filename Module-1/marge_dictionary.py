0bd0acfbf2010b906b9e0c48fc7b9f560fed25a2
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-09-11 13:30:26 +0600

    marge two dictionary

---
+++ Module-1/marge_dictionary.py
@@ -0,0 +1,6 @@
+class_five_students ={'sakib':9,'kadir':8,'molla':7}
+class_six_students = {'molla':8,'kadir':9,'imam':10,'rabbi':9.5}
+
+marge_students = class_five_students.update(class_six_students)
+
+print(class_five_students)
