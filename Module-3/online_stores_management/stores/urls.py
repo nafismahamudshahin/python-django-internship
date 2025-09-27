79c5dcd9f2fa5b062b759784be3313cf5d4b3ade
Nafis Mahamud Shahin <nafismahamudshahin@gmail.com>
2025-09-28 04:16:59 +0600

    make home, about and contact templates

--- Module-3/online_stores_management/stores/urls.py
+++ Module-3/online_stores_management/stores/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from stores.views import home,about,contact
 urlpatterns = [
-    
+    path('home/',home),
+    path('about/',about),
+    path('contact/',contact),
 ]
