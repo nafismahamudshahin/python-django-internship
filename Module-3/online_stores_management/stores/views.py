4d2f72c88236bdba214dd444d64d929d19352499
Nafis Mahamud Shahin <104847010+nafismahamudshahin@users.noreply.github.com>
2025-10-03 22:12:31 +0600

    Make dashboard view

--- Module-3/online_stores_management/stores/views.py
+++ Module-3/online_stores_management/stores/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
-
+def dashboard(request):
+    pass
 def about(request):
     return render(request,'about.html')
 
